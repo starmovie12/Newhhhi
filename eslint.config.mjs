1003cf075068c2f1c6c9ddf27a0e1f674adbafcd
starmovie12 <64631955+starmovie12@users.noreply.github.com>
2026-02-22 17:13:50 +0530

    Upload eslint.config.mjs via GitFolder Uploader

---
+++ eslint.config.mjs
@@ -0,0 +1,9 @@
+import { FlatCompat } from '@eslint/eslintrc'
+
+const compat = new FlatCompat()
+
+const eslintConfig = [
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+]
+
+export default eslintConfig
